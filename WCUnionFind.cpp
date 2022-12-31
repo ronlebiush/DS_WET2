aa5d24132da1cb5feaf8900c59f1c082afbdf290
ron lebiush <lebiush1@gmail.com>
2022-12-31 14:35:19 +0200

    unionfind

--- WCUnionFind.cpp
+++ WCUnionFind.cpp
@@ -3,3 +3,149 @@
 //
 
 #include "WCUnionFind.h"
+
+#include <utility>
+
+void WCUnionFind::unionTeams(int buyerTeamId, int boughtTeamId)
+{
+    std::shared_ptr<Team> buyerTeam = m_teams->findValue(buyerTeamId);
+    std::shared_ptr<Team> boughtTeam = m_teams->findValue(boughtTeamId);
+
+    PlayerNode* buyerRootPlayer= buyerTeam->getRootPlayer();
+    PlayerNode* boughtRootPlayer= boughtTeam->getRootPlayer();
+
+    permutation_t newTeamSpirit = boughtTeam->getSpirit()*buyerTeam->getSpirit();
+    int newPlayedGames = boughtRootPlayer->m_player->getGamesPlayed()-buyerRootPlayer->m_player->getGamesPlayed();
+    if (buyerRootPlayer->size > boughtRootPlayer->size) //B->A
+    {
+        boughtRootPlayer->m_dady = buyerRootPlayer;
+        boughtRootPlayer->m_player->setGamesPlayed(newPlayedGames);
+        //r(b) += h(a) -r(a)
+        permutation_t newSpirit = buyerRootPlayer->m_player->getSubSpirit().inv() *buyerRootPlayer->m_team->getSpirit() * boughtRootPlayer->m_player->getSubSpirit();
+        boughtRootPlayer->m_player->setSubSpirit(newSpirit);
+        buyerRootPlayer->size += boughtRootPlayer->size;
+        buyerRootPlayer->m_team->setSpirit(buyerRootPlayer->m_team->getSpirit()*boughtRootPlayer->m_team->getSpirit());
+        boughtRootPlayer->m_team.reset();
+
+    }
+    else //A->B
+    {
+        buyerRootPlayer->m_dady = boughtRootPlayer;
+        buyerRootPlayer->m_player->setGamesPlayed(-newPlayedGames);
+        //r(a) -= r(b)
+        //r(b) += H(a)
+        buyerRootPlayer->m_player->setSubSpirit(boughtRootPlayer->m_player->getSubSpirit().inv() *buyerRootPlayer->m_player->getSubSpirit());
+        boughtRootPlayer->m_player->setSubSpirit(boughtRootPlayer->m_player->getSubSpirit() *buyerRootPlayer->m_team->getSpirit());
+        boughtRootPlayer->size += buyerRootPlayer->size;
+        buyerRootPlayer->m_team->setSpirit(buyerRootPlayer->m_team->getSpirit()*boughtRootPlayer->m_team->getSpirit());
+        boughtRootPlayer->m_team.reset();
+        boughtRootPlayer->m_team = buyerRootPlayer->m_team;
+    }
+    buyerTeam->setSpirit(newTeamSpirit);
+}
+
+
+
+
+
+void WCUnionFind::removeTeam(int teamId){
+    PlayerNode* playerNode =m_teams->findValue(teamId)->getRootPlayer();
+    if (playerNode)
+        playerNode->m_team = nullptr;
+    m_teams->remove(teamId);
+}
+
+void WCUnionFind::insertTeam(int teamId,std::shared_ptr<Team> team){
+    m_teams->insert(teamId,team);
+}
+
+
+
+std::shared_ptr<Player> WCUnionFind::getPlayer(int playerId){
+    return m_players.getPlayer(playerId);
+
+}
+
+void WCUnionFind::insertPlayer(int teamId,int playerId, std::shared_ptr<Player> player)
+{
+    m_players.insert( playerId,  std::move(player));   //O(1)
+    PlayerNode* playerNode = m_players.getNode(playerId);       //O(1)
+    std::shared_ptr<Team> playerTeam = m_teams->findValue(teamId); //O(log k)
+    PlayerNode* rootPlayer = playerTeam->getRootPlayer();
+    if (!rootPlayer){
+        playerNode->m_dady = nullptr;
+        playerTeam->setRootPlayer(playerNode);
+        playerTeam->setSpirit(playerNode->m_player->getSubSpirit());
+    }
+    else{
+        permutation_t newTeamSpirit = playerTeam->getSpirit()*playerNode->m_player->getSubSpirit();
+        rootPlayer->size++;
+        playerNode->m_dady = rootPlayer;
+        playerNode->m_player->setGamesPlayed(playerNode->m_player->getGamesPlayed()-rootPlayer->m_player->getGamesPlayed());
+        //r(b) += h(a) -r(a)
+        permutation_t newSpirit = rootPlayer->m_player->getSubSpirit().inv() *rootPlayer->m_team->getSpirit() * playerNode->m_player->getSubSpirit();
+        playerNode->m_player->setSubSpirit(newSpirit);
+        playerTeam->setSpirit(newTeamSpirit);
+    }
+
+}
+
+
+
+std::shared_ptr<Team> WCUnionFind::findPlayersTeam(int playerId) {
+    PlayerNode *playerNode = m_players.getNode(playerId);
+    if (!playerNode)
+        return nullptr;
+
+    int gamesSum = 0;
+    permutation_t spiritSum = permutation_t::neutral();
+    PlayerNode *nodeIterator = playerNode;
+    while (nodeIterator->m_dady)//sum branch
+    {
+        gamesSum += nodeIterator->m_player->getGamesPlayed();
+        spiritSum = spiritSum * nodeIterator->m_dady->m_player->getSubSpirit();
+        playerNode = nodeIterator->m_dady;
+    }
+    PlayerNode *rootNode = nodeIterator;
+    nodeIterator = playerNode;
+
+    while (nodeIterator->m_dady) {
+        int currentPlayerGames = nodeIterator->m_player->getGamesPlayed();
+        permutation_t currentPlayerSpirit = nodeIterator->m_player->getSubSpirit();
+
+        nodeIterator->m_player->setGamesPlayed(gamesSum);
+        nodeIterator->m_player->setSubSpirit(spiritSum);
+
+        nodeIterator->m_dady = rootNode;
+
+        gamesSum -= currentPlayerGames;
+        spiritSum = spiritSum * currentPlayerSpirit.inv();
+    }
+    return rootNode->m_team;
+
+}
+
+
+
+output_t<int> WCUnionFind::num_played_games_for_player(int playerId){
+    //Find player in array
+    if (!m_players.Contains(playerId))
+        return StatusType::FAILURE;
+    findPlayersTeam(playerId);
+    PlayerNode* playerNode = m_players.getNode(playerId);
+    if(playerNode->m_dady)
+        return playerNode->m_player->getGamesPlayed()+playerNode->m_dady->m_player->getGamesPlayed();
+    return playerNode->m_player->getGamesPlayed();
+
+}
+output_t<permutation_t> WCUnionFind::get_partial_spirit(int playerId){
+    if (!m_players.Contains(playerId))
+        return StatusType::FAILURE;
+    findPlayersTeam(playerId);
+    PlayerNode* playerNode = m_players.getNode(playerId);
+    if(playerNode->m_dady)
+        return playerNode->m_player->getSubSpirit()*playerNode->m_dady->m_player->getSubSpirit();
+    return playerNode->m_player->getSubSpirit();
+}
+
+
--- WCUnionFind.h
+++ WCUnionFind.h
@@ -4,20 +4,43 @@
 
 #ifndef WET2_WCUNIONFIND_H
 #define WET2_WCUNIONFIND_H
+#include <utility>
+
 #include "AVLTree.h"
 #include "Player.h"
 #include "Team.h"
-#include "HashMap.h"
+#include "PlayersHashTable.h"
+//#include "HashMap.h"
+#include "PlayerNode.h"
+
+//class PlayerNode{
+//public:
+//    PlayerNode* m_dady;
+//    std::shared_ptr<Team> m_team;
+//    std::shared_ptr<Player> m_player;
+//    int size;
+//    PlayerNode* m_next;
+//    explicit PlayerNode(std::shared_ptr<Player> player): m_dady(nullptr), m_team(nullptr),m_player(std::move(player)), size(1),m_next(
+//            nullptr){}
+//};
+
+
 
 class WCUnionFind {
 private:
-    AVLTree<int,Team>* sets;
-    HashMap<int,Player>* players;
+    AVLTree<int,std::shared_ptr<Team>>* m_teams; //sets
+    PlayersHashTable m_players;
+    //HashMap<int,std::shared_ptr<Player>>* m_players; //items
 public:
     std::shared_ptr<Team> findPlayersTeam(int playerId); //the UF find func
-    void unionTeam(int team1 , int team2); //the UF Union func
-    void insertPlayer(const Player& player,int playerId);
-    void insertTeam(const Team& team,int teamId);
+    void unionTeams(int buyerTeamId, int boughtTeamId); //the UF Union func
+    void insertTeam(int teamId,std::shared_ptr<Team> team);
+    void removeTeam(int teamId);
+    void insertPlayer(int teamId,int playerId, std::shared_ptr<Player> player);
+    std::shared_ptr<Player> getPlayer(int playerId);
+    output_t<int> num_played_games_for_player(int playerId);
+    output_t<permutation_t> get_partial_spirit(int playerId);
+
 
 
 };
